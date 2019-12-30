40bfc67515d38ccc4d81619101815930107a7e24
vaijrb <vaijrb@gmail.com>
2019-12-29 20:31:54 -0500

    Py Sample Project
    Py Sample Project

---
+++ newsagg/news/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+import requests
+from bs4 import BeautifulSoup as BSoup
+from news.models import Headline
+
+def scrape(request):
+    session = requests.Session()
+    session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
+    url = "https://theonion.com"
+
+    content = session.get(url, verify = False).content
+    soup = BSoup(content,"html.parser")
+    News = soup.findall('div', {"class":"curation-module__item"})
+    
+    for article in News:
+        main = article.findall('a')[0]
+        link = main['href']
+        image_src = str(main.find('img')['srcset']).split(" ")[-4]
+        title = main['title']
+        new_headline = Headline()
+        new_headline.title = title
+        new_headline.url = link
+        new_headline.image = image_src
+        new_headline.save()
+
+    return redirect("../")
+
+def news_list(request):
+    headlines = Headline.__class__.objects.all()[::-1]
+    context = {
+        'object_list':headlines
+    }
+    return render(request, "news/home.html",context)
